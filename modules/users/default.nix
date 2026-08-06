1a63ce3df621a97a1a06ec010362e80ae01010cc
loonbac <loonbac@users.noreply.github.com>
2026-08-06 16:13:40 -0500

    fix: npm global en ~/.npm-global (prefix + PATH)

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -11,4 +11,16 @@
     # Paquetes instalados SOLO para este usuario (home-manager se integra aquí).
     packages = with pkgs; [ ];
   };
+
+  # npm global: instala en ~/.npm-global (el prefix del store de Nix es
+  # inmutable y no se puede escribir). Crea el dir y lo pone en el PATH.
+  systemd.tmpfiles.rules = [
+    "d /home/loonbac/.npm-global 0755 loonbac users -"
+    "d /home/loonbac/.npm-global/lib 0755 loonbac users -"
+    "d /home/loonbac/.npm-global/bin 0755 loonbac users -"
+  ];
+
+  environment.sessionVariables = {
+    PATH = [ "$HOME/.npm-global/bin" ];
+  };
 }
